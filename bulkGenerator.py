9195a00e218abfe56df2f68da757e63f70e03e53
catindog <gim.kobayashi@gmail.com>
2017-05-02 16:11:15 +0900

    f

---
+++ bulkGenerator.py
@@ -0,0 +1,89 @@
+
+from __future__ import print_function
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation
+from keras.layers import LSTM, GRU, SimpleRNN
+from keras.layers.core import Dropout
+from keras.optimizers import RMSprop, Adam
+from keras.utils.data_utils import get_file
+from keras.layers.normalization import BatchNormalization as BN
+from keras.callbacks import LearningRateScheduler as LRS
+import numpy as np
+import random
+import sys
+import glob
+import pickle
+import re
+import datetime
+
+def sample(preds, temperature=1.0):
+  preds = np.asarray(preds).astype('float64')
+  preds = np.log(preds) / temperature
+  exp_preds = np.exp(preds)
+  preds = exp_preds / np.sum(exp_preds)
+  probas = np.random.multinomial(1, preds, 1)
+  return np.argmax(probas)
+
+""" 一括で評価に使うデータセットを生成する """
+def bulkGenerate():
+  now   = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
+  S_NUM = 100
+  with open("./tmp/step4.pkl", "rb") as f:
+    char_vec = pickle.loads(f.read())
+
+  with open("./tmp/char_index.pkl", "rb") as f:
+    char_index = pickle.loads(f.read())
+    index_char = { index:char for char, index in char_index.items() }
+   
+  model_name = sorted(glob.glob("models/*.model"))[-1]
+
+  model = load_model(model_name)
+  
+  negatives = []
+  for it in range(S_NUM):
+    sent  = ["*"]*5
+    diversity = 1.0
+    next_char = None
+    negative  = []
+    for i in range(100):
+      if next_char is None or next_char != "*":
+        xs         = list(map(lambda x:char_vec[x], sent))
+        preds      = model.predict(np.array([xs]), verbose=0)[0]
+        next_index = sample(preds, diversity)
+        next_char  = index_char[next_index]
+        sent.append(next_char)
+        sent = sent[1:] 
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+        negative.append(next_char)
+        if next_char == "*":
+          ...
+    print()
+    negatives.append( ("0", "".join(negative).replace("*", "")) )
+
+  positives = []
+  ps = list(filter(lambda x:x!="", open("./tmp/step2", "r").read().split("\n")))
+  random.shuffle(ps)
+  for p in ps:
+    if "#" in p: continue
+    if len(p.split()) < 50: continue
+    if len(positives) > S_NUM:
+      continue
+    if len(re.findall(r"[a-z]", p)) > 20:
+      continue
+    p = p.replace("*", "").replace(" ", "")
+    positives.append( ("1", p) )
+    #print(p)
+      
+  target = negatives
+  target.extend(positives)
+  random.shuffle(target)
+  with open("./tmp/sample_{now}.txt".format(now=now), "w") as f:
+    for t in target:
+      print("__SEP__".join(list(t)))
+      f.write( "__SEP__".join(list(t)) + "\n" )
+
+
+if __name__ == '__main__':
+  bulkGenerate()
+  
--- rnn_pretrain.py
+++ rnn_pretrain.py
@@ -12,6 +12,8 @@ import random
 import sys
 import glob
 import pickle
+import re
+
 
 def build_model(mode=None, maxlen=None, output_dim=None):
   print('Build model...')
@@ -25,7 +27,7 @@ def build_model(mode=None, maxlen=None, output_dim=None):
     return model.optimizer.lr
   change_lr = LRS(_scheduler)
   model = Sequential()
-  model.add(LSTM(128*10, return_sequences=False, input_shape=(maxlen, 512)))
+  model.add(GRU(128*20, return_sequences=False, input_shape=(maxlen, 512)))
   model.add(Dense(output_dim))
   model.add(Activation('softmax'))
   optimizer = Adam()
@@ -67,11 +69,20 @@ def train(mode):
         print("now iter {counter}".format(counter=counter), file=sys.stderr)
       line = line.strip()
       chars = line.split()
+
       """ ある程度の長文を対象にしたいので、50字未満はスキップする """
-      if len(chars) < 70:
+      if len(chars) < 50:
         continue
       """ また、長すぎる文章もスキップしたいので、80字より大きいはスキップする """
       if len(chars) > 100:
+        continue
+      
+      """ 英単語を覗いて10文字より多いは消す """
+      if len(re.findall(r"[a-z]", line)) > 20:
+        continue
+
+      """ システム投稿にハッシュが入っていることが多いので、ハッシュを無視する """
+      if "#" in chars:
         continue
       #print(chars)
       for i in range(0, len(chars) - 6):
@@ -82,7 +93,7 @@ def train(mode):
           continue
 
         """ 特定の確率でデータセットに入れない """
-        if random.random() > 0.20: 
+        if random.random() > 0.30: 
           continue
         #print(window)
         """ ここからベクトル化 """
@@ -105,7 +116,7 @@ def train(mode):
     MODEL_NAME = "./models/%09d.model"%(iteration)
     model.save(MODEL_NAME)
     if iteration%1==0:
-      for diversity in [0.8, 1.0, 1.2]:
+      for diversity in [0.8, 1.0, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
         sent = ["*"]*5
@@ -124,54 +135,6 @@ def train(mode):
           except KeyError as e:
             break
         print()
-
-def eval():
-  INPUT_NAME = "./source/bocchan.txt"
-  MODEL_NAME = "./models/%s.model"%(INPUT_NAME.split('/').pop())
-
-  #path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
-  #text = open(path).read().lower()
-  text = open(INPUT_NAME).read()
-  print('corpus length:', len(text))
-
-  chars = sorted(list(set(text)))
-  print('total chars:', len(chars))
-  char_indices = dict((c, i) for i, c in enumerate(chars))
-  indices_char = dict((i, c) for i, c in enumerate(chars))
-  maxlen = 40
-  step = 3
-  sentences = []
-  next_chars = []
-  for i in range(0, len(text) - maxlen, step):
-    sentences.append(text[i: i + maxlen])
-    next_chars.append(text[i + maxlen])
-  model = load_model(MODEL_NAME)
-
-  #for diversity in [0.2, 0.5, 1.0, 1.2]:
-  for diversity in [1.0, 1.2]:
-    print()
-    print('----- diversity:', diversity)
-    generated = ''
-    start_index = random.randint(0, len(text) - maxlen - 1)
-    sentence = text[start_index: start_index + maxlen]
-    generated += sentence
-    print('----- Generating with seed: "' + sentence + '"')
-    sys.stdout.write(generated)
-    #for i in range(400):
-    for i in range(200):
-      x = np.zeros((1, maxlen, len(chars)))
-      for t, char in enumerate(sentence):
-        x[0, t, char_indices[char]] = 1.
-      preds = model.predict(x, verbose=0)[0]
-      #next_index = sample(preds, diversity)
-      next_index = dynast(preds, diversity)
-      next_char = indices_char[next_index]
-      generated += next_char
-      sentence = sentence[1:] + next_char
-      sys.stdout.write(next_char)
-      sys.stdout.flush()
-    print()
-
 
 def main():
   if '--train' in sys.argv:
